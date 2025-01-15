4c426126118b173c32c7de93792175f72c6a34bb
Sudipta Das <shiningsudipto@gmail.com>
2025-01-15 09:51:32 +0600

    while and nested loops practiced

---
+++ conditionsAndLoops/doWhile.cpp
@@ -0,0 +1,17 @@
+// do {
+//     // Code to execute
+// } while (condition);
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << "i = " << i << endl;
+        i++;
+    } while (i <= 5);
+    return 0;
+}
---
+++ conditionsAndLoops/while.cpp
@@ -0,0 +1,17 @@
+// while (condition) {
+//     // Code to execute
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << "i = " << i << endl;
+        i++;
+    }
+    return 0;
+}
