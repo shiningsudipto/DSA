8a418d04223fabedb4a995c86e1b1182943f0e2a
Sudipta Das <shiningsudipto@gmail.com>
2025-01-16 11:52:22 +0600

    character in patter shape showing

---
+++ patterns/squareAlphabet.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    char ch = 'A';
+    for (int i = 1; i <= n; i++)
+    {
+        // char ch = 'A';
+        for (int j = 1; j <= n; j++)
+        {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
