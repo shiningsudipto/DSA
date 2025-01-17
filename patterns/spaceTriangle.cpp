eef2e5c4f9cfd9edda871bf6acf353b473f56c11
Sudipta Das <shiningsudipto@gmail.com>
2025-01-17 14:39:06 +0600

    space triangle problem practiced

---
+++ patterns/spaceTriangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        // numbers
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << i + 1; // perfect triangle: << " "
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
